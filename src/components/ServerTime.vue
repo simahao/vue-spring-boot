df3149cbaa959581f9dfd47c33ebba5ec9aa5121
simahao <simahao@126.com>
2024-07-15 22:50:18 +0800

    add severtime

---
+++ src/components/ServerTime.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <button @click="fetchTime">Get Server Time</button>
+        <p v-if="serverTime">Server Time: {{ serverTime }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'app',
+    data() {
+        return {
+            serverTime: null
+        }
+    },
+    methods: {
+        fetchTime() {
+            axios.get('/api/curtime')
+                .then(response => {
+                    this.serverTime = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+    }
+}
+</script>
